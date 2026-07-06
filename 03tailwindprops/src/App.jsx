1090db5add076055a94ebb35747acb5eb9a6536d
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-07-06 10:47:03 +0530

    commit08

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="bg-green-400 text-white text-4xl p-6">
+      Tailwind is Working!
+    </h1>
+  );
+}
+
+export default App;
