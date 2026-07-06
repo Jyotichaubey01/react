19d4403d5c1d0029a3d6fdb021631c5c3f09ba77
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-07-06 09:32:42 +0530

    commit07

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(15);
 
- let [counter, setCounter] = useState(15)
- // let counter = 15
-  const addValue =() =>{
-   counter = counter+1
-  setCounter(counter)
-    
-  }
+  const addValue = () => {
+    setCounter((prev) => Math.min(20, prev + 1));
+  };
+
   const removeValue = () => {
-    setCounter(counter-1)
-  }
-  
+    setCounter((prev) => Math.max(0, prev - 1));
+  };
+
   return (
     <>
-      <h1>chai ur react</h1>
-      <h2>counter value: {counter}</h2>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value: {counter}</h2>
 
-      <button onClick={addValue}
-      >add value{counter}</button>
-       <br />
-       <button onClick={removeValue}
-       >remove value{counter}</button>   
+      <button onClick={addValue}>
+        Add Value
+      </button>
+
+      <br />
+      <br />
+
+      <button onClick={removeValue}>
+        Remove Value
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
